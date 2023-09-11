ff65551c80daf169c78a9cac66db3de9566c1e96
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-09-11 19:35:37 -0400

    test button prompts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+
+  var userChoice = confirm("TEST");
+  var userChoice2 = confirm("TEST 2");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +16,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Lowercase
+// Uppercase
+// Numeric
+// Special Characters
