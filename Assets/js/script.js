199b30da17825be118f2b5bcd5b4520f66cbc856
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-09-11 19:46:15 -0400

    add custom prompts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,10 +6,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
   // True/False box selectors that will allow the user to select if they want a certain criteria for their password or not
-  var choiceLowercase = confirm("TEST");
-  var choiceUpercase = confirm("TEST 2");
-  var choiceNumeric = confirm("TEST");
-  var choiceSpecialCharater = confirm("TEST");
+  var choiceLowercase = confirm("If you want to require lowercase characters, click 'Ok', if not, click 'Cancel'.");
+  var choiceUpercase = confirm("If you want to require uppercase characters, click 'Ok', if not, click 'Cancel'.");
+  var choiceNumeric = confirm("If you want to require numeric characters, click 'Ok', if not, click 'Cancel'.");
+  var choiceSpecialCharater = confirm("If you want to require special characters, click 'Ok', if not, click 'Cancel'.");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
