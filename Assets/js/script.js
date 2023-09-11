f4a4f86cee4558d48b5716827493f38e0f1fa658
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-09-11 19:39:34 -0400

    create variables for prompts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,8 +5,11 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  var userChoice = confirm("TEST");
-  var userChoice2 = confirm("TEST 2");
+  // True/False box selectors that will allow the user to select if they want a certain criteria for their password or not
+  var choiceLowercase = confirm("TEST");
+  var choiceUpercase = confirm("TEST 2");
+  var choiceNumeric = confirm("TEST");
+  var choiceSpecialCharater = confirm("TEST");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
