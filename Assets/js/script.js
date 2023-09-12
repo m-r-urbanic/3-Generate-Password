ea1ca474c643df866343b4f422656cf2a6d9e4c0
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-09-11 20:14:13 -0400

    display message based on if something is selected or not

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,17 +14,19 @@ function writePassword() {
   // Note: binary confirmation box chosen because it reduces the amount of potential inputs, and reduces the question to a simple 'yes/no' with no oppertunity for errors.
   // Made prompts into a function so that they can be called again if the user does not select at least one set of criteria
   function passwordPrompts(){
+    window.alert("Please choose if your password should require lowercase, uppercase, numeric, or special characters. Your password should require at least one type.");
     choiceLowercase = confirm("If you want to require lowercase characters, click 'Ok', if not, click 'Cancel'.");
     choiceUppercase = confirm("If you want to require uppercase characters, click 'Ok', if not, click 'Cancel'.");
     choiceNumeric = confirm("If you want to require numeric characters, click 'Ok', if not, click 'Cancel'.");
     choiceSpecialCharater = confirm("If you want to require special characters, click 'Ok', if not, click 'Cancel'.");
   }
 
-  // Call function
+  // Call password type prompts
   passwordPrompts();
 
+  // Validate that the user chose at least one prompt type
   function promptResult(){
-  if (choiceLowercase == true)
+  if (choiceLowercase == true || choiceUppercase == true)
   {
     window.alert("sometext");
   }
